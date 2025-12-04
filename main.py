cf5076ae526f4358f4a6a2cbd0b24df45952315e
srikantande <84713962+srikantande@users.noreply.github.com>
2025-12-04 19:39:20 +0530

    Update license from AGPL-3.0 to GPL-3.0

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-## GNU Affero General Public License v3.0 (AGPL-3.0)
-# Copyright 2024-2025 Drishyata
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-#
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+## GNU General Public License v3.0 (GPL-3.0)
+## https://www.gnu.org/licenses/gpl-3.0.en.html
 
 import streamlit as st
 import redis
